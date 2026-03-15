ef094af2bc3d0c3cd1f71deb3daae1a4d92b4b0a
irakozeaaron2005-dotcom <irakozeaaron2005@gmail.com>
2026-03-15 07:28:15 +0200

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,11 @@
+import telebot
+
+TOKEN = 8774207469:AAEjAL7CyT9c25cznEd7EoECWySJ-j-UQy8
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, "Gold Signal Bot is running 🚀")
+
+bot.infinity_polling()
