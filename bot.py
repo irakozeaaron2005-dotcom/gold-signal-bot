53281eae2c4606df4affef966c64edc343eb0230
irakozeaaron2005-dotcom <irakozeaaron2005@gmail.com>
2026-03-15 09:39:07 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,4 +8,8 @@ bot = telebot.TeleBot(TOKEN)
 def start(message):
     bot.reply_to(message, "Gold Signal Bot is running 🚀")
 
+@bot.message_handler(commands=['signal'])
+def signal(message):
+    bot.reply_to(message, "📊 XAUUSD SIGNAL\nBUY 🟢\nTP: 10 pips\nSL: 5 pips")
+
 bot.infinity_polling()
